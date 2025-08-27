b64b179c1bf509a0ee75b0515ef33f1e75fbfdf5
Jonathan <coxjonathan2016@gmail.com>
2025-08-26 21:51:59 -0600

    Initial Commit

--- exploration/graphs.py
+++ exploration/graphs.py
@@ -6,7 +6,7 @@ transaction_data = pandas.read_csv('../data/raw_data.csv')
 average_amount_by_customer = pandas.read_csv('../data/avg_transaction_by_customer.csv')
 fraud_transactions = transaction_data[transaction_data['Fraud_Label'] == 1]
 
-# Graph 1 - Bar Graph: Fraud Count by Merchant Type
+# VISUALIZATION 1 - Bar Graph: Fraud Count by Merchant Type
 fraud_count_by_merchant_type = fraud_transactions.groupby('merchant_type')
 fraud_count_by_merchant_type = fraud_count_by_merchant_type['transaction_id'].count()
 
@@ -19,6 +19,6 @@ percent_fraud_by_merchant_type = percent_fraud_by_merchant_type.sort_values(asce
 print(percent_fraud_by_merchant_type)
 percent_fraud_by_merchant_type.plot(x='merchant_type', kind='bar')
 plt.show()
-# Graph 2 - Line Plot: Fraud Count by Month
+# VISUALIZATION 2 - Line Plot: Fraud Count by Month
 
-# Graph 3 - Correlation Matrix
+# VISUALIZATION 3 - Correlation Matrix
