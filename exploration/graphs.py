1da11abd3fdce6179a16fd073e725b846d97acf5
Jonathan <coxjonathan2016@gmail.com>
2025-08-28 23:29:50 -0600

    Simplified code for Graph 1

--- exploration/graphs.py
+++ exploration/graphs.py
@@ -11,16 +11,11 @@ average_amount_by_customer = pandas.read_csv('data/avg_transaction_by_customer.c
 fraud_transactions = transaction_data[transaction_data['Fraud_Label'] == 1]
 
 # VISUALIZATION 1 - Bar Graph: Fraud Count by Merchant Type
-fraud_count_by_merchant_type = fraud_transactions.groupby('merchant_type')
-fraud_count_by_merchant_type = fraud_count_by_merchant_type['transaction_id'].count()
-
-transaction_count_by_merchant_type = transaction_data.groupby('merchant_type')
-transaction_count_by_merchant_type = transaction_count_by_merchant_type['transaction_id'].count()
-
+fraud_count_by_merchant_type = fraud_transactions.groupby(fraud_transactions['merchant_type'])['Fraud_Label'].sum()
+transaction_count_by_merchant_type = transaction_data.groupby('merchant_type')['transaction_id'].count()
 percent_fraud_by_merchant_type = fraud_count_by_merchant_type / transaction_count_by_merchant_type
-percent_fraud_by_merchant_type = percent_fraud_by_merchant_type.sort_values(ascending=False)
 
-print(percent_fraud_by_merchant_type)
+percent_fraud_by_merchant_type = percent_fraud_by_merchant_type.sort_values(ascending = False)
 percent_fraud_by_merchant_type.plot(x='merchant_type', kind='bar')
 plt.show()
 
@@ -28,7 +23,7 @@ plt.show()
 fraud_count_by_month = fraud_transactions.groupby(fraud_transactions['timestamp'].dt.month)['Fraud_Label'].sum() # https://www.statology.org/pandas-group-by-month/
 transaction_count_by_month = transaction_data.groupby(transaction_data['timestamp'].dt.month)['timestamp'].count()
 percent_fraud_by_month = fraud_count_by_month / transaction_count_by_month
-print(percent_fraud_by_month)
+
 percent_fraud_by_month.plot(x = 'timestamp')
 plt.show()
 
